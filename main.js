79c6f46bf28e41190c410f906f2679440cd382e6
Ahmed Mohamed <saikoahmed041@gmail.com>
2024-09-22 02:30:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+  let img= document.querySelector(".img")
+  let c =document.querySelector(".container")
+  function ph(x){
+    img.src =x
+  }
+
+  function color(i){
+    c.style.background= i
+  }
